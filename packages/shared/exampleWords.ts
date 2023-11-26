ec69e88b9ffaa5b72d307f9c0de2584bd2435e65
Alex Patterson <alex@codingcat.dev>
2023-11-25 22:14:51 -0500

    init

---
+++ packages/shared/exampleWords.ts
@@ -0,0 +1,66 @@
+export const words = [
+    {
+      "theme": "playful",
+      "words": ["tabby", "pounce", "whisker", "bounce"]
+    },
+    {
+      "theme": "elegant",
+      "words": ["siamese", "regal", "grace", "elegant"]
+    },
+    {
+      "theme": "curious",
+      "words": ["curious", "prowl", "inquisitive", "nosey"]
+    },
+    {
+      "theme": "adorable",
+      "words": ["kitten", "cute", "snuggle", "purr"]
+    },
+    {
+        "theme": "furry",
+        "words": ["fluffy", "soft", "paws", "whiskers"]
+      },
+      {
+        "theme": "mysterious",
+        "words": ["shadow", "enigma", "cloak", "sphinx"]
+      },
+      {
+        "theme": "charming",
+        "words": ["grin", "twinkle", "magnetic", "enchant"]
+      },
+      {
+        "theme": "swift",
+        "words": ["zoom", "fleet", "dart", "nimble"]
+      },
+      {
+        "theme": "whiskers",
+        "words": ["feline", "whisker", "prowl", "pounce"]
+      },
+      {
+        "theme": "cuddly",
+        "words": ["snuggle", "cozy", "soft", "warm"]
+      },
+      {
+        "theme": "mysterious",
+        "words": ["sphinx", "shadow", "enigma", "mystery"]
+      },
+      {
+        "theme": "graceful",
+        "words": ["grace", "elegant", "gentle", "sleek"]
+      },
+      {
+        "theme": "EnergeticExplorer",
+        "words": ["playful", "adventurous", "explorer", "energetic"]
+      },
+      {
+        "theme": "SnuggleMaster",
+        "words": ["cuddly", "cozy", "snuggler", "warm"]
+      },
+      {
+        "theme": "GourmetGourmand",
+        "words": ["tasty", "delicious", "gourmet", "fancy"]
+      },
+      {
+        "theme": "Purr-fectlyContent",
+        "words": ["happy", "content", "serene", "purr-fect"]
+      },
+]
