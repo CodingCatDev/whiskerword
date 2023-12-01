e77b152efbf79f9fee25cefcb647cc16aa7d4d3f
Alex Patterson <alex@codingcat.dev>
2023-12-01 12:01:43 -0800

    add firebase

--- apps/whiskerword-svelte-firebase/src/routes/+layout.svelte
+++ apps/whiskerword-svelte-firebase/src/routes/+layout.svelte
@@ -6,44 +6,68 @@
 	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
 	import { storePopup } from '@skeletonlabs/skeleton';
 	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+
+	import { FirebaseApp } from 'sveltefire';
+	import { initializeApp } from 'firebase/app';
+	import { getFirestore } from 'firebase/firestore';
+	import { getAuth } from 'firebase/auth';
+	import { getStorage } from 'firebase/storage';
+
+	// Initialize Firebase
+	const app = initializeApp({
+		apiKey: 'AIzaSyDtA64NUU5MSm8EI0UNHcHp8oGOvVlTLRA',
+		authDomain: 'whiskerword.firebaseapp.com',
+		projectId: 'whiskerword',
+		storageBucket: 'whiskerword.appspot.com',
+		messagingSenderId: '801717146769',
+		appId: '1:801717146769:web:cf68e38b9eece755305b30',
+		measurementId: 'G-MRREFE81N0'
+	});
+	const firestore = getFirestore(app);
+	const auth = getAuth(app);
+	const storage = getStorage(app);
 </script>
 
-<!-- App Shell -->
-<AppShell>
-	<svelte:fragment slot="header">
-		<!-- App Bar -->
-		<AppBar>
-			<svelte:fragment slot="lead">
-				<strong class="text-xl uppercase">Skeleton</strong>
-			</svelte:fragment>
-			<svelte:fragment slot="trail">
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://discord.gg/EXqV7W8MtY"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Discord
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://twitter.com/SkeletonUI"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Twitter
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://github.com/skeletonlabs/skeleton"
-					target="_blank"
-					rel="noreferrer"
-				>
-					GitHub
-				</a>
-			</svelte:fragment>
-		</AppBar>
-	</svelte:fragment>
-	<!-- Page Route Content -->
-	<slot />
-</AppShell>
+<!-- Firebase App -->
+
+<FirebaseApp {auth} {firestore} {storage}>
+	<!-- App Shell -->
+	<AppShell>
+		<svelte:fragment slot="header">
+			<!-- App Bar -->
+			<AppBar>
+				<svelte:fragment slot="lead">
+					<strong class="text-xl uppercase">Skeleton</strong>
+				</svelte:fragment>
+				<svelte:fragment slot="trail">
+					<a
+						class="btn btn-sm variant-ghost-surface"
+						href="https://discord.gg/EXqV7W8MtY"
+						target="_blank"
+						rel="noreferrer"
+					>
+						Discord
+					</a>
+					<a
+						class="btn btn-sm variant-ghost-surface"
+						href="https://twitter.com/SkeletonUI"
+						target="_blank"
+						rel="noreferrer"
+					>
+						Twitter
+					</a>
+					<a
+						class="btn btn-sm variant-ghost-surface"
+						href="https://github.com/skeletonlabs/skeleton"
+						target="_blank"
+						rel="noreferrer"
+					>
+						GitHub
+					</a>
+				</svelte:fragment>
+			</AppBar>
+		</svelte:fragment>
+		<!-- Page Route Content -->
+		<slot />
+	</AppShell>
+</FirebaseApp>
---
+++ apps/whiskerword-svelte-firebase/src/routes/login/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { SignedIn, SignedOut } from 'sveltefire';
+	import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+</script>
+
+<section class="flex h-full w-full justify-center items-center">
+	<div>
+		<SignedOut let:auth>
+			<button
+				class="btn variant-filled-primary"
+				on:click={() => signInWithPopup(auth, new GoogleAuthProvider())}>Sign In</button
+			>
+		</SignedOut>
+
+		<SignedIn let:user let:signOut>
+			<div class="flex flex-col gap-2">
+				<p>Hello {user?.displayName}</p>
+				<button class="btn variant-filled-secondary" on:click={signOut}>Sign Out</button>
+			</div>
+		</SignedIn>
+	</div>
+</section>
